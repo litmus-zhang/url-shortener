f83ed4ee0a95e7c0c7c77dd35ea4fe8eb13cd409
litmus-zhang <abdulsalamlukmon9@gmail.com>
2025-04-30 21:46:49 +0100

    Refactor form handling in the URL shortener: replace HTML form with Remix Form component, update action method, and improve input placeholder text.

--- frontend/app/routes/_index.tsx
+++ frontend/app/routes/_index.tsx
@@ -1,5 +1,5 @@
 import type { ActionFunctionArgs, MetaFunction } from '@remix-run/node';
-import { Link, redirect } from '@remix-run/react';
+import { Link, redirect, Form } from '@remix-run/react';
 
 export const meta: MetaFunction = () => {
   return [
@@ -8,38 +8,51 @@ export const meta: MetaFunction = () => {
   ];
 };
 
-export const action = async ({
-  request,
-}: ActionFunctionArgs) => {
+export const action = async ({ request }: ActionFunctionArgs) => {
   const formData = await request.formData();
-  const data = Object.fromEntries(formData);
+  const url = formData.get('url');
+
+  if (!url || typeof url !== 'string') {
+    throw new Error('URL is missing or invalid.');
+  }
+
+  await fetch('http://localhost:3000/api/encode', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ url }),
+  });
 
   return redirect(`/listing`);
 };
 
 export default function Index() {
-  // const urls = useLoaderData<typeof loader>();
   return (
     <div className="flex flex-col gap-3 h-screen items-center justify-center">
-      <Link className='underline' to="/listing">All Urls &rarr;</Link>
+      <Link className="underline" to="/listing">
+        All Urls &rarr;
+      </Link>
 
-      <form
+      <Form
         className="flex flex-col items-center justify-start gap-2 p-10 bg-gray-700 rounded shadow-md"
-        action=""
         id="url-form"
-        method='post'
+        method="post"
+        action='/encode'
       >
-        <h1 className='font-bold text-lg my-2'>URL Shortener</h1>
+        <h1 className="font-bold text-lg my-2">URL Shortener</h1>
         <input
           type="text"
           aria-label="url"
           name="url"
           id="url"
           className="p-2 border rounded"
-          placeholder="pastelong url here"
+          placeholder="paste long url here"
         />
-        <button type='submit' className='bg-blue-500 px-4 py-2 rounded hover:bg-blue-300'> Shorten Url </button>
-      </form>
+        <button type="submit" className="bg-blue-500 px-4 py-2 rounded hover:bg-blue-300">
+          Shorten Url
+        </button>
+      </Form>
     </div>
   );
 }
